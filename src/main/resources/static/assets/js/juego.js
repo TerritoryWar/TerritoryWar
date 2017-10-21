2a9b07dc0d681aa4f99084dc70f30b2e2628f1c3
Juan Camilo Herrera <juan.herrera@mail.escuelaing.edu.co>
2017-10-21 17:34:03 -0500

    arreglando problema de movimiento de naves sin posibilidades

--- src/main/resources/static/assets/js/juego.js
+++ src/main/resources/static/assets/js/juego.js
@@ -4,129 +4,130 @@
  * and open the template in the editor.
  */
 var Juego = (function () {
-    var usuario=null; //el objeto usuario
+    var usuario = null; //el objeto usuario
     var stompClient = null;
-    var tablero=[];
-    var lenTablero=8;
-    var naveSeleccionada=false; //booleano para saber si el jugador tiene una nave seleccionada
-    var posiblesMovimientos=[];
+    var tablero = [];
+    var lenTablero = 8;
+    var naveSeleccionada = false; //booleano para saber si el jugador tiene una nave seleccionada
+    var posiblesMovimientos = [];
     var posAnterior;
-    
-    var genFila= function(numFila){
-        var ans="";
-        for (var i = 0; i < 8; i++) { 
-            ans+="<td style='padding:0 0 0 0 !important;'>\n\
-                    <button id='"+i+"-"+numFila+"' onclick='Juego.oprimiBoton("+i+","+numFila+")' style='width:100%;'>\n\
+
+    var genFila = function (numFila) {
+        var ans = "";
+        for (var i = 0; i < 8; i++) {
+            ans += "<td style='padding:0 0 0 0 !important;'>\n\
+                    <button id='" + i + "-" + numFila + "' onclick='Juego.oprimiBoton(" + i + "," + numFila + ")' style='width:100%;'>\n\
                         <img src='/images/vacio.png' height='100%' width='100%'/>\n\
                     </button>\n\
                   </td>";
         }
-        return ans;    
+        return ans;
     };
-    
-    var genTodasFilas= function(){
-        var ans="";
+
+    var genTodasFilas = function () {
+        var ans = "";
         for (var i = 0; i < 8; i++) {
-            ans+="<tr >\n\
-                        "+genFila(i)+"\n\
+            ans += "<tr >\n\
+                        " + genFila(i) + "\n\
                         </tr>";
         }
         return ans;
     };
-    
-    var genNavesAliadas = function(){
+
+    var genNavesAliadas = function () {
         //Por el momento solo phoenix
-        for (var i = lenTablero-2; i < lenTablero; i++) {
+        for (var i = lenTablero - 2; i < lenTablero; i++) {
             for (var j = 0; j < lenTablero; j++) {
-                tablero[j][i]=new Phoenix(j,i,"aliado");
+                tablero[j][i] = new Phoenix(j, i, "aliado");
             }
         }
     };
-    
-    var genNavesEnemigas = function(){
+
+    var genNavesEnemigas = function () {
         //Por el momento solo phoenix
         for (var i = 0; i < 2; i++) {
             for (var j = 0; j < lenTablero; j++) {
-                tablero[j][i]=new Phoenix(j,i,"enemigo");
+                tablero[j][i] = new Phoenix(j, i, "enemigo");
             }
         }
     };
-    
-    
-    var isArrayInArray = function (arr, item){
+
+
+    var isArrayInArray = function (arr, item) {
         var item_as_string = JSON.stringify(item);
 
-        var contains = arr.some(function(ele){
-          return JSON.stringify(ele) === item_as_string;
+        var contains = arr.some(function (ele) {
+            return JSON.stringify(ele) === item_as_string;
         });
         return contains;
-      };
-    
-    
+    };
+
+
     return {
-      generarTablero:function (){
-          $("#panelJuego").css('display','inherit');
-          $("#panelJuego").html(
-                  "<div class='container'>\n\
+        generarTablero: function () {
+            $("#panelJuego").css('display', 'inherit');
+            $("#panelJuego").html(
+                    "<div class='container'>\n\
                         <div class='row'>\n\
  -                            <div class='col-md-3 col-md-offset-100'></div>\n\
  -                            <table style='table-layout:fixed;'>\n\
-                                "+genTodasFilas()+"\
+                                " + genTodasFilas() + "\
                             </table>\n\
                             <div ></div>\n\
                         </div>\n\
                    </div>"
-                  );
-          for (var i = 0; i < lenTablero; i++) {
-                tablero[i]=[];
+                    );
+            for (var i = 0; i < lenTablero; i++) {
+                tablero[i] = [];
             }
-          genNavesAliadas();
-          genNavesEnemigas();
-          
-      },
-      oprimiBoton:function (x,y){
-          if(!naveSeleccionada){ //la nave no ha sido seleccionada
-              if(tablero[x][y]!==null && tablero[x][y].getBando()==="aliado"){
-                  try{ 
-                    posiblesMovimientos=tablero[x][y].getPosiblesMovimientos(tablero);
-                    
-                    naveSeleccionada=true;
-                    posAnterior=[x,y];
-                      for (var i = 0; i < posiblesMovimientos.length; i++) {
-                          var x1 = posiblesMovimientos[i][0];
-                          var y1 = posiblesMovimientos[i][1];
-                          if (tablero[x1][y1]!== undefined && tablero[x1][y1]!== null && tablero[x1][y1].getBando()==="enemigo"){
-                              $("#"+x1+"-"+y1).html("<img src='/images/PhoenixEnemigoKill.png' height='100%' width='100%'/>");
-                          }else{
-                              $("#"+x1+"-"+y1).html("<img src='/images/posibleMovimiento.png' height='100%' width='100%'/>");
-                          }
-                          
-                      }
-                  }catch(e){
-                      console.log(e);
-                  }
+            genNavesAliadas();
+            genNavesEnemigas();
 
-              }
-          }
-          else{
-              if(isArrayInArray(posiblesMovimientos,[x,y])){//que si se puede mover a dicha posicion
-                  for (var i = 0; i < posiblesMovimientos.length; i++) {
-                      var x1 = posiblesMovimientos[i][0];
-                          var y1 = posiblesMovimientos[i][1];
-                          if (tablero[x1][y1]!== undefined && tablero[x1][y1]!== null && tablero[x1][y1].getBando()==="enemigo"){
-                              $("#"+x1+"-"+y1).html("<img src='/images/PhoenixEnemigo.png' height='100%' width='100%'/>");
-                          }else{
-                              $("#"+x1+"-"+y1).html("<img src='/images/vacio.png' height='100%' width='100%'/>");
-                          }
-                      
-                      }
-                  tablero[posAnterior[0]][posAnterior[1]].moverNave(x,y);
-                  tablero[x][y]=tablero[posAnterior[0]][posAnterior[1]];
-                  tablero[posAnterior[0]][posAnterior[1]]=null;
-                  naveSeleccionada=false;
-              }
-          }
-          
-      }
+        },
+        oprimiBoton: function (x, y) {
+            if (!naveSeleccionada) { //la nave no ha sido seleccionada
+                if (tablero[x][y] !== null && tablero[x][y].getBando() === "aliado") {
+                    try {
+                        posiblesMovimientos = tablero[x][y].getPosiblesMovimientos(tablero);
+                        
+                        if (posiblesMovimientos.length >= 1) {
+                            naveSeleccionada = true;
+                            posAnterior = [x, y];
+                            for (var i = 0; i < posiblesMovimientos.length; i++) {
+                                var x1 = posiblesMovimientos[i][0];
+                                var y1 = posiblesMovimientos[i][1];
+                                if (tablero[x1][y1] !== undefined && tablero[x1][y1] !== null && tablero[x1][y1].getBando() === "enemigo") {
+                                    $("#" + x1 + "-" + y1).html("<img src='/images/PhoenixEnemigoKill.png' height='100%' width='100%'/>");
+                                } else {
+                                    $("#" + x1 + "-" + y1).html("<img src='/images/posibleMovimiento.png' height='100%' width='100%'/>");
+                                }
+                            }
+                        }
+
+                    } catch (e) {
+                        console.log(e);
+                    }
+
+                }
+            } else {
+                if (isArrayInArray(posiblesMovimientos, [x, y])) {//que si se puede mover a dicha posicion
+                    for (var i = 0; i < posiblesMovimientos.length; i++) {
+                        var x1 = posiblesMovimientos[i][0];
+                        var y1 = posiblesMovimientos[i][1];
+                        if (tablero[x1][y1] !== undefined && tablero[x1][y1] !== null && tablero[x1][y1].getBando() === "enemigo") {
+                            $("#" + x1 + "-" + y1).html("<img src='/images/PhoenixEnemigo.png' height='100%' width='100%'/>");
+                        } else {
+                            $("#" + x1 + "-" + y1).html("<img src='/images/vacio.png' height='100%' width='100%'/>");
+                        }
+
+                    }
+                    tablero[posAnterior[0]][posAnterior[1]].moverNave(x, y);
+                    tablero[x][y] = tablero[posAnterior[0]][posAnterior[1]];
+                    tablero[posAnterior[0]][posAnterior[1]] = null;
+                    naveSeleccionada = false;
+                }
+            }
+
+        }
     };
 })();
