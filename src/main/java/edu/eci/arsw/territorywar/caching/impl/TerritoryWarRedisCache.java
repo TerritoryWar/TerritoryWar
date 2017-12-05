68aad2a41a54570049b8ab128b8efac6e9a0b9f7
esteban7700 <jhordy.salinas@mail.escuelaing.edu.co>
2017-12-05 13:41:13 -0500

    agregando redis, hay que probarlo con la maquina

--- src/main/java/edu/eci/arsw/territorywar/caching/impl/TerritoryWarRedisCache.java
+++ src/main/java/edu/eci/arsw/territorywar/caching/impl/TerritoryWarRedisCache.java
@@ -5,10 +5,67 @@
  */
 package edu.eci.arsw.territorywar.caching.impl;
 
+import edu.eci.arsw.territorywar.caching.TerritoryWarCache;
+import edu.eci.arsw.territorywar.exceptions.TerritoryWarException;
+import edu.eci.arsw.territorywar.model.Jugador;
+import edu.eci.arsw.territorywar.model.Partida;
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+
 /**
  *
  * @author carlo
  */
-public class TerritoryWarRedisCache {
+//@Service
+public class TerritoryWarRedisCache implements TerritoryWarCache{
+    @Autowired
+    private StringRedisTemplate template;
+    
+//    RedisScript<String> script;
+
+    @Override
+    public Set<Partida> getPartidasDisponibles() {
+        Set<Partida> ans = new HashSet<>();
+        Set<String>  partidas = template.opsForSet().members("partidas");
+        for (String partida : partidas) {
+            if(template.opsForHash().get("partida:" + partida, "jugador2").equals("null")){
+                Partida par = new Partida((String) partida,new Jugador((String)template.opsForHash().get("partida:" + partida, "jugador1"), null,null,null));
+                ans.add(par);
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public void crearPartida(Jugador jugador) {
+        template.opsForHash().put("partida:" + jugador.getId(), "jugador1 ",jugador.getId());
+    }
+
+    @Override
+    public void unirAPartida(String id, Jugador jugador2) throws TerritoryWarException {
+        template.opsForHash().put("partida:" + id, "jugador2 ",jugador2.getId());
+    }
+
+    @Override
+    public Partida getPartida(String id) {
+        Partida ans;
+        ans =  new Partida(id , new Jugador((String)template.opsForHash().get("partida:" + id, "jugador1"), null, null, null));
+        if(!template.opsForHash().get("partida:" + id, "jugador2").equals("null")){
+            try {
+                ans.setJugador2(new Jugador((String)template.opsForHash().get("partida:" + id, "jugador2"),null,null,null));
+            } catch (TerritoryWarException ex) {
+                ex.getMessage();
+            }
+        }
+        return ans;
+    }
+
+    @Override
+    public void deletePartida(String idPartida) {
+        template.opsForHash().delete("partida:" + idPartida);
+    }
     
 }
